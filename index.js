95f7eb39e347a361279ce8ab3bab622d4a50fc8b
João Victor de F. Lima <jvdfreitas@gmail.com>
2024-09-30 23:07:31 -0300

    Finish Project

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function inserirTarefas(listaDeTarefas) {
           <h5>${tarefa.titulo}</h5>
           <p>${tarefa.descricao}</p>
           <div class="actions">
-            <box-icon name='trash' size="sm"></box-icon>
+            <box-icon name='trash' size="sm" onClick=deletarTarefa(${tarefa.id})></box-icon>
           </div>
         </li>
       `;
@@ -51,6 +51,34 @@ function novaTarefa() {
     .then((res) => {
       fecharModal();
       buscarTarefas();
+      let form = document.querySelector("#criarTarefa form")
+      form.reset();
     });
-  
+}
+
+function deletarTarefa(id) {
+  fetch(`http://localhost:3000/tarefas/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      buscarTarefas();
+    });
+}
+
+function pesquisarTarefas() {
+  let lis = document.querySelectorAll("ul li");
+  if (busca.value.length > 0) {
+    lis.forEach((li) => {
+      if (!li.children[1].innerText.includes(busca.value)) {
+        li.classList.add("oculto");
+      } else {
+        li.classList.remove("oculto");
+      }
+    });
+  } else {
+    lis.forEach((li) => {
+      li.classList.remove("oculto");
+    });
+  }
 }
